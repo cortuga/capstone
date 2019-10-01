4b06db485ca9eef5d738e343faa4816ec7b4db85
Corey Grandmaison <cgsustainability@gmail.com>
2019-09-30 22:46:29 -0400

    refactored Todos, but getting error

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import React, { Component } from "react"
 import Todos from "./components/Todos"
-import "bulma/css/bulma.css"
+// import "bulma/css/bulma.css"
 
 class App extends Component {
-  state = {
-    todos: [
-      {
-        id: Math.random() + 1,
-        title: "Take out the trash",
-        completed: false
-      },
-      {
-        id: 2,
-        title: "Dinner",
-        completed: false
-      },
-      {
-        id: 3,
-        title: "Meeting with boss",
-        completed: true
-      }
-    ]
-  }
+  // state = {
+  //   todos: [
+  //     {
+  //       id: Math.random() + 1,
+  //       title: "Take out the trash",
+  //       completed: false
+  //     },
+  //     {
+  //       id: 2,
+  //       title: "Dinner",
+  //       completed: false
+  //     },
+  //     {
+  //       id: 3,
+  //       title: "Meeting with boss",
+  //       completed: true
+  //     }
+  //   ]
+  // }
   render() {
     return (
       <>
         <div>
-          <Todos todos={this.state.todos} />
+          <Todos />
         </div>
       </>
     )
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,24 +4,42 @@ import react, { useState } from 'react'
 export default function Todos() {
 
     const [newTodo, setNewTodo] = useState('')
-    // const [] = useState([])
+    const [todos, setTodos] = useState([
+      { id: 1, text: 'default_todos_text'}
+    ])
 
 
     function handleNewTodoChange(e) {
         e.preventDefault()
-        setNewTodo(e.target)
+        setNewTodo(e.target.value)
+    }
+
+    function handleNewTodo(e) {
+      e.preventDefault()     //grabs the current state of newTodo
+      if (newTodo === '') return   //if newtodo is empty then return and do nothing
+      setTodos([...todos, {id: Date.now(), text: newTodo}])
+      e.target.reset() 
+      // console.log(newTodo) 
+    }
+
+    function removeTodo(id) {
+      setTodos(todos.filter((todo) => todo.id != id))
     }
 
     return (
         <>
             <h1>Todos</h1>
-            <form>
+            <form onSubmit={handleNewTodo}>
 
                 <input placeholder="Your todo..." 
                 onChange={handleNewTodoChange}></input>
                 
                 <ul>
-                    <li>buy milk</li>
+                    {todos.map((todo) => (
+                      <li key={todo.id}> {todo.text}
+                      <button onclick={() => removeTodo(todo.id)}>x</button>
+                      </li>
+                    ))}
                 </ul>
 
             </form>
