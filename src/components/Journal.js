26d2c0f356316ada9848f1956145a7dcce522ca1
Corey Grandmaison <cgsustainability@gmail.com>
2019-09-30 22:50:08 -0400

    Journal.js experimentation

---
+++ src/components/Journal.js
@@ -0,0 +1,12 @@
+import React from 'react';const Journal = () => {
+    return (
+      <>
+        <form>
+        {/* form needs onSubmit={this.handleSubmit} */}
+          <label>
+            <textarea value={} onChange={} />
+          </label>
+        </form>
+      </>
+    );
+   }export default Journal;
