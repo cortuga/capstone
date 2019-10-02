26b563924b7f9dffdccf827a685cf1707ebe5bf9
Corey Grandmaison <cgsustainability@gmail.com>
2019-10-02 19:36:21 -0400

    Routes set up in app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react"
+import Nav from "./components/Nav"
+import Journal from "./components/Journal"
 import Todos from "./components/Todos"
+import Calender from "./components/Calender"
+import LifeGoals from "./components/LifeGoals"
+import Profile from "./components/Profile"
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 // import "bulma/css/bulma.css"
 
 class App extends Component {
@@ -25,11 +32,25 @@ class App extends Component {
   render() {
     return (
       <>
-        <div>
-          <Todos />
-        </div>
+        <Router>
+          <Nav>
+            <Switch>
+              <Route path='/' component={Home} />
+              <Route path='/Journal' component={Journal} />
+              <Route path='/Todos' component={Todos} />
+              <Route path='/Calender' component={Calender} />
+              <Route path='/LifeGoals' component={LifeGoals} />
+              <Route path='/Profile' component={Profile} />
+            </Switch>
+          </Nav>
+        </Router>
       </>
     )
   }
 }
+const Home = () => (
+  <>
+    <h1>Home Page</h1>
+  </>
+)
 export default App
